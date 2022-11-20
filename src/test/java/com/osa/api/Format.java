702a2c5a2df2f10873850121201e2a516f942b0d
mahfuz <20mahfuzahmed20@gmail.com>
2022-11-20 13:06:57 -0500

    adding payload

--- src/test/java/com/osa/api/Format.java
+++ src/test/java/com/osa/api/Format.java
@@ -1,14 +1,24 @@
 package com.osa.api;
 
+import java.util.HashMap;
+
 import org.json.JSONObject;
+
+import com.osa.utility.Utilities;
 
 import io.restassured.response.Response;
 
 public class Format {
 	public static void main(String[] args) {
-		System.out.println(prettyPrint("p:abc"));
+		HashMap<String,String> hm=new HashMap<String,String>();
+	    hm.put("firstName", "qatar");
+	    hm.put("lastName","oman");
+	    hm.put("phone","11192022");
+	    hm.put("email", "football@gmail.com");
+	    String payload=Utilities.getJsonObject(hm).toString();
+		System.out.println(prettyPrint(payload));
 	}
-
+	
 	public static String prettyPrint(Response res) {
 		JSONObject response=new JSONObject(res.asString());
 		return response.toString(4);	
